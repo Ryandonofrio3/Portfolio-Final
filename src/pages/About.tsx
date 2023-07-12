549f1cc0133b149ce7eddc553008ca62b24bb68d
Ryandonofrio3 <128195293+research-radar@users.noreply.github.com>
2023-07-12 10:22:00 -0700

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -21,11 +21,35 @@ const About: React.FC = () => {
                 </h2>
 
                 <p className="text-3xl text-white text-left">
-                    I am Ryan D'Onofrio, a PhD candiate in Dynamical Neuroscience at UC Santa Barbara. <br /> <br />  Throughout this journey, I've uncovered a passion for the intersection of software development and scientific exploration <br /> <br />
-                    Armed with creative problem-solving abilities, an innovative mindset, and a deep-seated passion for advancing our world through science, I am excited to
+                    I am Ryan D'Onofrio, a PhD candiate in Dynamical Neuroscience at UC Santa Barbara. <br /> <br />  Throughout this journey, I've uncovered a passion for building Artificial Intelligence workflows to enhance scientific exploration <br /> <br />
+                    Armed with creative problem-solving abilities, and an entrepreneurial spirit I am excited to
 
                     be a part of the upcoming technological revolution
                 </p>
+                <br />
+                <br />
+                <p className="text-3xl text-white text-left">Some technologies I have worked with include:</p>
+                <div style={{ display: "flex" }}>
+                    <div style={{ flex: "1" }}>
+                        <br />
+                        <ul className="text-2xl text-left justify-left text-white">
+                            <li> * Python</li>
+                            <li> * R/RStudio</li>
+                            <li>* CRISPR-Cas9</li>
+                            <li>* React/TailwindCSS</li>
+                        </ul>
+                    </div>
+                    <div style={{ flex: "1", marginLeft: "20px", marginTop: "25px" }}>
+                        <ul className="text-2xl text-left justify-left text-white">
+
+                            <li>* Infusion Cloning</li>
+                            <li>* OpenAI API</li>
+                            <li>* MGDrive</li>
+                            <li>* DeepSqueak</li>
+                        </ul>
+                    </div>
+                </div>
+
             </div >
         </div >
     );
